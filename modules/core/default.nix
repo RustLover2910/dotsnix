351492627d202371e56e5661c3e053ba5fbc36bf
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-10 18:43:59 +0630

    Updated fish aliases

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,30 +1,33 @@
-{ inputs, nixpkgs, self, username, ... }:
-let
+{
+  inputs,
+  nixpkgs,
+  self,
+  username,
+  ...
+}: let
   system = "x86_64-linux";
-  flake_path = "/home/${username}/Document/nixdots/";
+  flake_path = "/home/${username}/dotsnix";
   pkgs = nixpkgs.legacyPackages.${system};
   lib = nixpkgs.lib;
-in
-{
+in {
   nathannix = lib.nixosSystem {
-    specialArgs = { inherit self inputs username flake_path; };
+    specialArgs = {inherit self inputs username flake_path;};
     modules =
-      [ (import ./bootloader.nix) ]
-      ++ [ (import ./hardware.nix) ]
-      ++ [ (import ./xserver.nix) ]
+      [(import ./bootloader.nix)]
+      ++ [(import ./hardware.nix)]
+      ++ [(import ./xserver.nix)]
       #++ [ (import ./steam.nix) ]
-      ++ [ (import ./network.nix) ]
-      ++ [ (import ./pipewire.nix) ]
-      ++ [ (import ./program.nix) ]
+      ++ [(import ./network.nix)]
+      ++ [(import ./pipewire.nix)]
+      ++ [(import ./program.nix)]
       #++ [ (import ./../home/package/python.nix) ]
-      ++ [ (import ./security.nix) ]
-      ++ [ (import ./services.nix) ]
-      ++ [ (import ./system.nix) ]
-      ++ [ (import ./user.nix) ]
-      ++ [ (import ./wayland.nix) ]
+      ++ [(import ./security.nix)]
+      ++ [(import ./services.nix)]
+      ++ [(import ./system.nix)]
+      ++ [(import ./user.nix)]
+      ++ [(import ./wayland.nix)]
       #++ [ (import ./cloudflare-warp.nix) ]
       # ++ [ (import ./virtualization.nix) ]
-      ++ [ (import ./../../hosts/nixos/hardware-configuration.nix) ]
-    ;
+      ++ [(import ./../../hosts/nixos/hardware-configuration.nix)];
   };
 }
--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
     shellInit = "scheme set catppuccin";
+    shellAliases = {
+      nix-sw = "sudo nixos-rebuild switch --flake ${flake_path}#${username}";
+      nix-up = "sudo nixos-rebuild switch --upgrade --flake ${flake_path}#${username}";
+      nix-fup = "sudo nix flake update ${flake_path}#";
+    };
+  };
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
   };
 }
