7869eabfa49b8460008fdf079b481c9fd0799e0e
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-10 19:01:27 +0630

    modified:   modules/core/default.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,34 +1,34 @@
-{ inputs
-, nixpkgs
-, self
-, username
-, ...
-}:
-let
+{
+  inputs,
+  nixpkgs,
+  self,
+  username,
+  flake_path,
+  ...
+}: let
   system = "x86_64-linux";
   #flake_path = "/home/${username}/dotsnix";
   pkgs = nixpkgs.legacyPackages.${system};
   lib = nixpkgs.lib;
-in
-{
+in {
   nathannix = lib.nixosSystem {
-    specialArgs = { inherit self inputs username; };
+    specialArgs = {inherit self inputs username flake_path;};
     modules =
-      [ (import ./bootloader.nix) ]
-      ++ [ (import ./hardware.nix) ]
-      ++ [ (import ./xserver.nix) ]
+      [(import ./bootloader.nix)]
+      ++ [(import ./hardware.nix)]
+      ++ [(import ./xserver.nix)]
       #++ [ (import ./steam.nix) ]
-      ++ [ (import ./network.nix) ]
-      ++ [ (import ./pipewire.nix) ]
-      ++ [ (import ./program.nix) ]
+      ++ [(import ./network.nix)]
+      ++ [(import ./pipewire.nix)]
+      ++ [(import ./program.nix)]
       #++ [ (import ./../home/package/python.nix) ]
-      ++ [ (import ./security.nix) ]
-      ++ [ (import ./services.nix) ]
-      ++ [ (import ./system.nix) ]
-      ++ [ (import ./user.nix) ]
-      ++ [ (import ./wayland.nix) ]
+      ++ [(import ./security.nix)]
+      ++ [(import ./services.nix)]
+      ++ [(import ./system.nix)]
+      ++ [(import ./user.nix)]
+      ++ [(import ./wayland.nix)]
       #++ [ (import ./cloudflare-warp.nix) ]
       # ++ [ (import ./virtualization.nix) ]
-      ++ [ (import ./../../hosts/nixos/hardware-configuration.nix) ];
+      ++ [(import ./../../hosts/nixos/hardware-configuration.nix)];
   };
 }
