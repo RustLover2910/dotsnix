47c109073f13bb3281b72c553c2437e2ae4b5167
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-10 13:13:21 +0630

    fwa

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -2,12 +2,6 @@
   programs = {
     dconf.enable = true;
     fish.enable = true;
-    #neovim = {
-   #   enable = true;
-    #  defaultEditor = true;
-    #  viAlias = true;
-    #  vimAlias = true;
-   # };
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
