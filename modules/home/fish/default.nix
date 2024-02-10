0866bbf8c5f13204c390925042befd174f3af6ac
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-10 19:32:51 +0630

    modified:   modules/home/fish/default.nix

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -1,16 +1,14 @@
-{
-  pkgs,
-  username,
-  flake_path,
-  ...
+{ pkgs
+, username
+, ...
 }: {
   programs.fish = {
     enable = true;
     shellInit = "scheme set catppuccin";
     shellAliases = {
-      nix-sw = "sudo nixos-rebuild switch --flake ${flake_path}#${username}";
-      nix-up = "sudo nixos-rebuild switch --upgrade --flake ${flake_path}#${username}";
-      nix-fup = "sudo nix flake update ${flake_path}#";
+      nix-sw = "sudo nixos-rebuild switch --flake /home/${username}/dotsnix#nathannix";
+      nix-up = "sudo nixos-rebuild switch --upgrade --flake /home/${username}/dotsnix#nathannix";
+      nix-fup = "sudo nix flake update /home/${username}/dotsnix#";
       cdnix = "cd ~/dotsnix && codium ~/dotsnix";
     };
   };
