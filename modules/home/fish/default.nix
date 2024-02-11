f565bbe7d92defb49d2c83c86876eed7567398f5
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-11 08:34:08 +0630

    modified:   modules/home/fish/default.nix

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -7,8 +7,13 @@
     shellInit = "scheme set catppuccin";
     shellAliases = {
       nix-sw = "sudo nixos-rebuild switch --flake /home/${username}/dotsnix#nathannix";
-      nix-up = "sudo nixos-rebuild switch --upgrade --flake /home/${username}/dotsnix#nathannix";
-      nix-fup = "sudo nix flake update /home/${username}/dotsnix#";
+      nix-up = "sudo nix flake update /home/${username}/dotsnix# && sudo nixos-rebuild switch --upgrade --flake /home/${username}/dotsnix#nathannix";
+      # nix-fup = "sudo nix flake update /home/${username}/dotsnix#";
+      cl = "clear";
+      re = "fish";
+      del = "sudo nix-collect-garbage -d ";
+      l = "eza --icons  -a --group-directories-first -1"; #EZA_ICON_SPACING=2
+      ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
       cdnix = "cd ~/dotsnix && codium ~/dotsnix";
     };
   };
