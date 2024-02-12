8366ffff9a490f8caf4a000b863f9c8c53b1609a
RustLover2910 <abyssalyrics2910@gmail.com>
2024-02-12 10:37:41 +0630

    deleted:    dotsnix/modules/home/qt/default.nix

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -13,21 +13,23 @@
   programs.fish = {
     enable = true;
     shellInit = " set -U fish_greeting && scheme set catppuccin";
-    shellAbbrs = {
-      nix-sw = "sudo nixos-rebuild switch --flake /home/${username}/dotsnix#nathannix";
-      nix-up = "sudo nix flake update /home/${username}/dotsnix# && sudo nixos-rebuild switch --upgrade --flake /home/${username}/dotsnix#nathannix";
-      # nix-fup = "sudo nix flake update /home/${username}/dotsnix#";
+    shellAliases = {
       cl = "clear";
       re = "fish";
-      del = "sudo nix-collect-garbage -d ";
       l = "eza --icons  -a -T --group-directories-first -1"; #EZA_ICON_SPACING=2
       ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
       cdnix = "z ~/dotsnix && codium ~/dotsnix";
       nvnix = "z ~/dotsnix && nvim ~/dotsnix";
     };
-  };
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
+    shellAbbrs = {
+      nix-sw = "sudo nixos-rebuild switch --flake /home/${username}/dotsnix#nathannix";
+      nix-up = "sudo nix flake update /home/${username}/dotsnix# && sudo nixos-rebuild switch --upgrade --flake /home/${username}/dotsnix#nathannix";
+      # nix-fup = "sudo nix flake update /home/${username}/dotsnix#";
+      del = "sudo nix-collect-garbage -d ";
+    };
+    programs.zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
   };
 }
