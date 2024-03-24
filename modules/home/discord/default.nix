e166c21c70ca1a3330f03b167b48ae755339fe68
RustLover2910 <abyssalyrics2910@gmail.com>
2024-03-24 21:12:27 +0630

    everforest

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
-  imports = [(import ./theme-template.nix)];
+  imports =
+    [(import ./theme-template.nix)]
+    ++ [(import ./everforest.nix)];
   home.packages = with pkgs; [
     (discord.override {
       #nss = pkgs.nss_3_90_2;
