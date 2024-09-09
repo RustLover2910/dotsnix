4fc264c047fd01cb53d3d5c33d43b5d16b85ef50
RustLover2910 <abyssalyrics2910@gmail.com>
2024-09-09 10:03:13 +0630

    remove disk

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -12,9 +12,9 @@
     supportedFilesystems = ["ntfs"];
     kernelPackages = pkgs.linuxPackages_latest;
   };
-  fileSystems."/home/${username}/MIGHTYBACKUP" = {
-    device = "/dev/disk/by-uuid/5ba1e8c1-020d-41f6-91a8-1941cc77de07";
-    fsType = "ext4";
-    options = ["defaults" "user"];
-  };
+  #fileSystems."/home/${username}/MIGHTYBACKUP" = {
+  # device = "/dev/disk/by-uuid/5ba1e8c1-020d-41f6-91a8-1941cc77de07";
+  # fsType = "ext4";
+  # options = ["defaults" "user"];
+  # };
 }
